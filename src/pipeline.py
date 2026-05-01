0a037891b7ab8193603dd3f32b00b92c11761fb0
nguyenvulong0509 <nguyenvulong0509@gmail.com>
2026-05-01 14:56:31 +0700

    Initial commit: pwl repo created

---
+++ src/pipeline.py
@@ -0,0 +1,32 @@
+import os
+import io
+import csv
+import duckdb
+from dotenv import load_dotenv
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+load_dotenv()
+folder_id = os.getenv('drive_folder_id')
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+DB_PATH = 'state/etl_state.duckdb'
+SILVER_DIR = 'data/silver/workouts/'
+
+def authenticate_gdrive():
+    """Handles Google Drive OAuth 2.0 authentication."""
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return build('drive', 'v3', credentials=creds)
